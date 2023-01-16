2aa32582df64f5e709c38b8728615c4ca974ab72
Lucas Lima <lucaslima.devs@gmail.com>
2023-01-16 09:17:02 -0300

    feat: Add GetFonecedoresById, PostFonecedor and miniValidator

--- DemoMinimalAPI/Models/Fornecedor.cs
+++ DemoMinimalAPI/Models/Fornecedor.cs
@@ -1,9 +1,13 @@
-﻿namespace DemoMinimalAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoMinimalAPI.Models
 {
     public class Fornecedor
     {
         public Guid Id { get; set; }
+        [Required, MinLength(4)]
         public string? Nome { get; set; }
+        [Required, MinLength(11)]
         public string? Documento { get; set; }
         public bool Ativo { get; set; }
     }
--- DemoMinimalAPI/Program.cs
+++ DemoMinimalAPI/Program.cs
@@ -1,5 +1,7 @@
 using DemoMinimalAPI.Data;
+using DemoMinimalAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using MiniValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +13,6 @@ builder.Services.AddDbContext<MinimalContextDb>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -24,6 +25,38 @@ app.MapGet("/fornecedor", async (
     MinimalContextDb context) =>
     await context.Fornecedores.ToListAsync())
     .WithName("GetFonecedores")
+    .WithTags("Fornecedor"); //tags name (similar controller name)
+
+app.MapGet("/fornecedor/{id}", async (
+    Guid id, 
+    MinimalContextDb context) =>
+    await context.Fornecedores.FindAsync(id)
+        is Fornecedor fornecedor ? //if exists fornecedor
+        Results.Ok(fornecedor) : 
+        Results.NotFound()
+    )
+    .Produces<Fornecedor>(StatusCodes.Status200OK) //documentation for swagger
+    .Produces<Fornecedor>(StatusCodes.Status404NotFound)
+    .WithName("GetFonecedoresById")
+    .WithTags("Fornecedor");
+
+app.MapPost("/fornecedor", async (
+    MinimalContextDb context,
+    Fornecedor fornecedor) =>
+{
+    if (!MiniValidator.TryValidate(fornecedor, out var errors)) //package MiniValidation, minimalistic validation library
+        return Results.ValidationProblem(errors);
+
+    await context.Fornecedores.AddAsync(fornecedor);
+    var result = await context.SaveChangesAsync();
+
+    return result > 0
+        ? Results.Created($"/fornecedor/{fornecedor.Id}", fornecedor)
+        : Results.BadRequest("Houve um problema ao salvar registro");
+})
+    .Produces<Fornecedor>(StatusCodes.Status201Created) //documentation for swagger
+    .Produces<Fornecedor>(StatusCodes.Status400BadRequest)
+    .WithName("PostFonecedor")
     .WithTags("Fornecedor");
 
 app.Run();
